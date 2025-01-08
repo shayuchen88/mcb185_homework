44ccc2735d5dcc7d201cf5778a0e596658310f2e
username <shayuchen88@gmail.com>
2025-01-08 11:36:48 -0800

    commit 20demo

---
+++ 20demo.py
@@ -0,0 +1,18 @@
+t = 1, 2
+print(t)
+print(type(t))
+
+person = 'Steve', 21, 57891.5
+print(person)
+
+def midpoint(x1, y1, x2, y2):
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    return x, y
+
+print(midpoint(1, 2, 3, 4))
+m = midpoint(1, 2, 3, 4)
+mx, my = midpoint(1, 2, 3, 4)
+print(mx, my)
+
+print(m[0], m[1])
