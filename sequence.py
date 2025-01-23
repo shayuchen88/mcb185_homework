2f55b5c365d958cd39535a5cd9969637c0ae4910
username <shayuchen88@gmail.com>
2025-01-23 15:53:15 -0800

    commit

--- sequence.py
+++ sequence.py
@@ -21,3 +21,12 @@ def translate(dna):
         elif codon == "TGA": ass.append("*")
         else: ass.append("X")
     return " ".join(ass)
+
+def gc_comp(seq):
+    return (seq.count("C") + seq.count("G")) / len(seq)
+
+def gc_skew(seq):
+    c = seq.count("C")
+    g = seq.count("G")
+    if c + g == 0: return 0
+    return (g - c) / (g + c)
--- test_sequence.py
+++ test_sequence.py
@@ -3,3 +3,13 @@ import sequence
 print(sequence.transcribe("ACGT"))
 print(sequence.revcomp("AAAACGT"))
 print(sequence.translate("ATGCCCTAA"))
+
+s = "ACGTGGGGGGCATATG"
+print(sequence.gc_comp(s))
+print(sequence.gc_skew(s), sequence.gc_skew(sequence.revcomp(s)))
+
+seq = "ACGTACGTGGGGGACGTACGTCCCCC"
+w = 10
+for i in range(len(seq) -w +1):
+    sq = seq[i::i+w]
+    print(i, sequence.gc_comp(sq), sequence.gc_skew(sq))
