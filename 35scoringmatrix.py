3ed63e30c6343173c613d027f7baf851089e10e7
username <shayuchen88@gmail.com>
2025-02-04 15:25:38 -0800

    commit

--- 35scoringmatrix.py
+++ 35scoringmatrix.py
@@ -5,9 +5,16 @@ nt = sys.argv[1]
 match = sys.argv[2]
 mismatch = sys.argv[3]
 
+def ma(nt1, nt2):
+    if nt1 == nt2:
+        return match
+    return mismatch
+
+for nts in nt:
+    print(f'{nts:>3}', end="")
+print()
 for i in range(0, len(nt)):
+    print(nt[i], end="")
     for j in range(0, len(nt)):
-        if nt[i] == nt[j]:
-            print(nt[i], match)
-        else: print(nt[j], mismatch)
-    
+        print(f'{(ma(nt[i], nt[j])):>3}', end="")
+    print()
